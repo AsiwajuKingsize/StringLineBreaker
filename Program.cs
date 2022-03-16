1a1b2ceca2ff8d1fda785aed9ab81abe36d653d3
Osun Temidayo Oluwasegun <kingsize.wood@gmail.com>
2022-03-16 15:30:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using System.Text;
+
+
+Console.WriteLine("Enter MaximumCharacter per line:");
+
+// Create a string variable and get user input from the keyboard and store it in the variable
+ReadInput:
+string maxStringLength = Console.ReadLine();
+int maxLineLength=0;
+
+if (!string.IsNullOrEmpty(maxStringLength))
+{
+    if (int.TryParse(maxStringLength, out maxLineLength))
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("Enter valid MaximumCharacter per line:");
+        //Console.ReadLine();
+        goto ReadInput;
+    }
+   // int maxLineLength = Convert.ToInt32(maxStringLength);
+}
+else
+{
+    Console.WriteLine("Enter valid  MaximumCharacter per line:");
+    goto ReadInput;
+}
+
+//Get input File from user computer MyDocuments special Folder
+string pathToFiles = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+string[] lines = File.ReadAllLines(Path.Combine(pathToFiles, "sampleStringInput.txt"));
+//Exceute function to format input string from a text file
+ formatSampleInput(lines, maxLineLength);
+
+ static void formatSampleInput(string[] inp, int len)
+{
+    int pos = 0;
+    StringBuilder outputString = new StringBuilder();
+    if (inp.Length < 1)
+    {
+        Console.WriteLine("Input string is Empty");
+    }
+    else
+    {
+        foreach (string line in inp)
+        {
+            if (line.Length > len)
+            {
+                //pos = len;
+                while(pos < (line.Length) - pos)
+                {
+                    outputString.AppendLine(line.Substring(pos,len));
+                    pos+=len;
+                    
+                }
+
+                if (pos+1 > len)
+                {
+                    outputString.AppendLine(line.Substring(pos));
+                }
+            }
+            else
+            {
+                outputString.AppendLine(line);
+            }
+            pos = 0;
+        }       
+    }
+    
+    Console.WriteLine(outputString.ToString());
+    string pathToFiles = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    using (StreamWriter outputFile = new StreamWriter(Path.Combine(pathToFiles, "SampleStringOutput.txt")))
+    {
+        outputFile.WriteLine(outputString.ToString());
+    }
+     //return outputString.ToString();
+
+  
+}
+
+
+
